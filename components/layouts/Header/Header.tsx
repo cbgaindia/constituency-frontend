5a3a82731faf586d13b3272537c50c853afe2c19
pixeledcode <ashoaib540918@gmail.com>
2022-03-09 17:11:39 +0530

    header update

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import HeaderComp from './HeaderComp';
+import styled from 'styled-components';
 
 const Header = ({ data }) => {
   return (
@@ -19,3 +19,36 @@ const Header = ({ data }) => {
 };
 
 export default Header;
+
+const HeaderComp = styled.div`
+  .container {
+    padding-top: 1rem;
+    display: block;
+    border-bottom: var(--separator-5-2);
+  }
+
+  h2 {
+    margin-top: 2rem;
+    font-size: 2rem;
+    font-weight: 500;
+    line-height: 2.6rem;
+    grid-column: 2/3;
+    grid-row: 1/2;
+    word-break: break-all;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 1.5;
+    grid-column: 2/3;
+    grid-row: 2/3;
+
+    &:first-of-type {
+      padding-top: 1rem;
+    }
+
+    &:last-child {
+      padding-bottom: 2rem;
+    }
+  }
+`;
