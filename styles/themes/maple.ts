ea3c1bb43f672da214cdf9d2fde3507d7923fc8b
aashimgarg <18bcs001@smvdu.ac.in>
2022-10-17 14:15:54 +0530

    add the required themes in global

--- styles/themes/maple.ts
+++ styles/themes/maple.ts
@@ -1,49 +1,56 @@
 import { css } from 'styled-components';
 
 export const maple = css`
-    --amazon_01: rgb(40, 160, 98);
-    --amazon_02: rgb(35, 142, 87);
-    --amazon_03: rgb(29, 117, 72);
-    --amazon_04: rgb(22, 87, 53);
-    --amazon_05: rgb(17, 69, 42);
-    --amazon_06: rgb(13, 51, 31);
-    --amazon_00: rgb(178, 210, 194);
-    --maple_01: rgb(233, 184, 64);
-    --maple_02: rgb(206, 163, 56);
-    --maple_03: rgb(170, 134, 46);
-    --maple_04: rgb(125, 99, 34);
-    --maple_05: rgb(99, 78, 26);
-    --maple_06: rgb(72, 57, 19);
-    --maple_00: rgb(231, 215, 176);
-    --teal_01: rgb(74, 190, 190);
-    --teal_02: rgb(65, 168, 168);
-    --teal_03: rgb(54, 139, 139);
-    --teal_04: rgb(40, 103, 103);
-    --teal_05: rgb(31, 81, 81);
-    --teal_06: rgb(23, 59, 59);
-    --olive_01: rgb(204, 190, 114);
-    --olive_02: rgb(180, 168, 101);
-    --olive_03: rgb(149, 139, 83);
-    --olive_04: rgb(109, 102, 61);
-    --olive_05: rgb(86, 80, 47);
-    --olive_06: rgb(62, 58, 34);
-    --flamingo_01: rgb(222, 121, 146);
-    --flamingo_02: rgb(196, 107, 129);
-    --flamingo_03: rgb(162, 88, 106);
-    --flamingo_04: rgb(119, 64, 78);
-    --flamingo_05: rgb(94, 50, 61);
-    --flamingo_06: rgb(68, 35, 44);
-    --grey_01: rgb(73, 77, 68);
-    --grey_02: rgb(102, 110, 106);
-    --grey_03: rgb(136, 143, 139);
-    --grey_04: rgb(171, 176, 173);
-    --grey_05: rgb(205, 209, 207);
-    --grey_06: rgb(239, 242, 241);
-    --grey_00: rgb(255, 255, 255);
-    --backgroundLightest: rgb(255, 255, 255);
-    --backgroundLight: rgb(235, 240, 238);
-    --backgroundDark: rgb(45, 82, 63);
-    --backgroundDarkest: rgb(23, 41, 32);
+    --color-amazon-100: rgb(40, 160, 98);
+    --color-amazon-200: rgb(35, 142, 87);
+    --color-amazon-300: rgb(29, 117, 72);
+    --color-amazon-400: rgb(22, 87, 53);
+    --color-amazon-500: rgb(17, 69, 42);
+    --color-amazon-600: rgb(13, 51, 31);
+    --color-amazon-00: rgb(178, 210, 194);
+
+    --color-maple-100: rgb(233, 184, 64);
+    --color-maple-200: rgb(206, 163, 56);
+    --color-maple-300: rgb(170, 134, 46);
+    --color-maple-400: rgb(125, 99, 34);
+    --color-maple-500: rgb(99, 78, 26);
+    --color-maple-600: rgb(72, 57, 19);
+    --color-maple-100: rgb(231, 215, 176);
+
+    -color-teal-100: rgb(74, 190, 190);
+    -color-teal-200: rgb(65, 168, 168);
+    -color-teal-300: rgb(54, 139, 139);
+    -color-teal-400: rgb(40, 103, 103);
+    -color-teal-500: rgb(31, 81, 81);
+    -color-teal-600: rgb(23, 59, 59);
+
+    --color-olive-100: rgb(204, 190, 114);
+    --color-olive-200: rgb(180, 168, 101);
+    --color-olive-300: rgb(149, 139, 83);
+    --color-olive-400: rgb(109, 102, 61);
+    --color-olive-500: rgb(86, 80, 47);
+    --color-olive-600: rgb(62, 58, 34);
+
+    --color-flamingo-100: rgb(222, 121, 146);
+    --color-flamingo-200: rgb(196, 107, 129);
+    --color-flamingo-300: rgb(162, 88, 106);
+    --color-flamingo-400: rgb(119, 64, 78);
+    --color-flamingo-500: rgb(94, 50, 61);
+    --color-flamingo-600: rgb(68, 35, 44);
+
+    --color-grey-100: rgb(73, 77, 68);
+    --color-grey-200: rgb(102, 110, 106);
+    --color-grey-300: rgb(136, 143, 139);
+    --color-grey-400: rgb(171, 176, 173);
+    --color-grey-500: rgb(205, 209, 207);
+    --color-grey-600: rgb(239, 242, 241);
+    --color-grey-00: rgb(255, 255, 255);
+
+    --color-background-lightest: rgb(255, 255, 255);
+    --color-background-light: rgb(235, 240, 238);
+    --color-background-dark: rgb(45, 82, 63);
+    --color-background-darkest: rgb(23, 41, 32);
+    
     --semanticError: rgb(205, 81, 61);
     --semanticSuccess: rgb(51, 131, 90);
     --semanticWarning: rgb(215, 170, 59);
