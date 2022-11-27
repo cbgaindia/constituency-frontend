b9d36be50b753195a8a867064659959f14997643
aashimgarg <18bcs001@smvdu.ac.in>
2022-11-28 01:22:22 +0530

    removed splitbar from state averages

--- components/viz/SplitBar.tsx
+++ components/viz/SplitBar.tsx
@@ -3,25 +3,39 @@ import styled from 'styled-components';
 
 const SplitBar = ({ val }) => {
   function changeBar(val) {
-    if (val >= 0) {
-      const barVal = val > 100 ? 100 : val;
-      chart1.current?.style.setProperty('transform', `translateX(100%)`);
-      chart2.current?.style.setProperty(
+    if(val==0){
+      chart.current?.style.setProperty(
         'transform',
-        `translateX(${barVal - 100}%)`
-      );
-    } else {
-      const barVal = val < -100 ? -100 : val;
-      chart2.current?.style.setProperty('transform', `translateX(-100%)`);
-      chart1.current?.style.setProperty(
-        'transform',
-        `translateX(${100 - Math.abs(barVal)}%)`
+        `translateX(-100%)`
       );
     }
+    else{
+      const barVal = val > 100 ? 100 : val;
+      console.log(barVal)
+      
+     // chart.current?.style.setProperty('transform', `translateX(100%)`);
+      {
+        val > 0  
+         ? chart.current?.style.setProperty('background-color', `var(--color-teal-100)`) 
+         : chart.current?.style.setProperty('background-color', `var(--color-flamingo-100)`)
+      }
+      chart.current?.style.setProperty(
+        'transform',
+        `translateX(${Math.abs(barVal) - 100}%)`
+      );
+    }
+  //    else {
+  //     const barVal = val < -100 ? -100 : val;
+  //  //   chart.current?.style.setProperty('transform', `translateX(100%)`);
+  //     chart.current?.style.setProperty(
+  //       'transform',
+  //       `translateX(${100 - Math.abs(barVal)}%)`
+  //     );
+  //   }
   }
-
-  const chart1: any = React.useRef();
-  const chart2: any = React.useRef();
+ 
+  const chart: any = React.useRef();
+ // const chart: any = React.useRef();
 
   React.useEffect(() => {
     changeBar(val);
@@ -29,11 +43,12 @@ const SplitBar = ({ val }) => {
 
   return (
     <Wrapper>
+      {/* <Bar>
+        <Chart ref={chart} className="chart-1" />
+      </Bar> */}
       <Bar>
-        <Chart ref={chart1} className="chart-1" />
-      </Bar>
-      <Bar>
-        <Chart ref={chart2} className="chart-2" />
+          <Chart ref={chart} className="chart" />
+       
       </Bar>
     </Wrapper>
   );
@@ -47,15 +62,15 @@ const Wrapper = styled.div`
   height: 20px;
 
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 4px;
+  //grid-template-columns: 1fr 1fr;
+  //gap: 4px;
 `;
 
 const Bar = styled.div`
   background-color: var(--color-grey-600);
   overflow: hidden;
   position: relative;
-  transform: translateZ(0px);
+  //transform: translateZ(0px);
 `;
 
 const Chart = styled.div`
@@ -63,13 +78,13 @@ const Chart = styled.div`
   height: 100%;
   transition: transform 660ms cubic-bezier(0.65, 0, 0.35, 1) 0s;
 
-  &.chart-1 {
+  /* &.chart-1 {
     background-color: var(--color-flamingo-100);
     transform: translateX(100%);
-  }
+  } */
 
-  &.chart-2 {
+  /* &.chart {
     background-color: var(--color-teal-100);
-    transform: translateX(-100%);
-  }
+     transform: translateX(100%); 
+  } */
 `;
