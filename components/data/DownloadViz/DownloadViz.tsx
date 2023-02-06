b785ec354afe3682d6b389fb2d6aabd94c9f0fd7
aashimgarg <18bcs001@smvdu.ac.in>
2023-02-06 18:22:48 +0530

    FIX: add domtoimg package to download viz

--- components/data/DownloadViz/DownloadViz.tsx
+++ components/data/DownloadViz/DownloadViz.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { saveAs } from 'file-saver';
+// import { saveAs } from 'file-saver';
 import { Download } from 'components/icons/Download';
 import { Box, Button } from '@opub-cdl/design-system';
 // import { Button } from 'components/actions';
+import domtoimage from 'dom-to-image';
 
 function fileName(meta) {
   // If there is no type, eg: table, don;t add it to the name
@@ -48,25 +49,6 @@ export function export_table_to_csv(tableData, filename: any) {
   download_csv(csv.join('\n'), filename);
 }
 
-function createDummyCanvas(srcCanvas) {
-  //create a dummy CANVAS
-  const destinationCanvas = document.createElement('canvas');
-  destinationCanvas.width = srcCanvas.width;
-  destinationCanvas.height = srcCanvas.height;
-
-  const destCtx = destinationCanvas.getContext('2d');
-
-  //create a rectangle with the desired color
-  destCtx.fillStyle = '#FFFFFF';
-  destCtx.fillRect(0, 0, srcCanvas.width, srcCanvas.height);
-
-  //draw the original canvas onto the destination canvas
-  destCtx.drawImage(srcCanvas, 0, 0);
-
-  //finally use the destinationCanvas.toDataURL() method to get the desired output;
-  return destinationCanvas.toDataURL('image/jpeg', 2);
-}
-
 type Props = {
   viz: string;
   meta: any;
@@ -75,35 +57,67 @@ type Props = {
 };
 
 const DownloadViz = ({ viz, meta, tableData = {}, source }: Props) => {
-  let watermarkSSR;
-  useEffect(() => {
-    import('watermarkjs').then((x) => (watermarkSSR = x.default));
-  }, [viz, meta]);
-  function svg2img(canvasElm) {
-    const myChart = createDummyCanvas(canvasElm);
 
-    watermarkSSR([myChart, '/assets/images/obi.png'])
-      .image(watermarkSSR.image.lowerRight(0.5))
-      .render()
-      .image(watermarkSSR.text.lowerLeft(`${"        "} Data Source ${source}`, '18px Josefin Slab', '#000', 0.5))
-      .then((img) => saveAs(img.src, `${fileName(meta)}.jpeg`.toLowerCase()));
-  }
+  const filterElements = (node) => {
+    try {
+      return (
+        node.getAttribute('id') !== 'hide-this-button' &&
+        node.getAttribute('class') !== 'statetooltip' &&
+        node.getAttribute('class') !== 'tcontainer' &&
+        node.getAttribute('class') !== 'select-container' &&
+        node.getAttribute('class') !== 'details__download' &&
+        node.nodeType != 8 &&
+        node.getAttribute('class') != 'see-details-text'
+      );
+    } catch (err) {
+      return true;
+    }
+  };
 
   async function downloadSelector(viz) {
     if (viz == '#tableView')
       export_table_to_csv(tableData, `${fileName(meta)}.csv`.toLowerCase());
     else {
-      await import('html2canvas')
-        .then((html2canvas) => {
-          html2canvas
-            .default(document.querySelector(viz), {
-              scale: 2,
-            })
-            .then((canvasElm) => svg2img(canvasElm));
+      
+      domtoimage
+        .toPng(document.querySelector(viz), {
+          filter: filterElements,
         })
-        .catch((e) => {
-          console.error('load failed');
+        .then((dataURL) => {
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+          const image = new Image();
+          image.src = dataURL;
+          
+          image.onload = function() {
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0);
+            const watermark = new Image();
+            watermark.src = '/assets/images/obi.png';
+            watermark.onload = function() {
+              const watermarkWidth = watermark.width * 0.5;
+              const watermarkHeight = watermark.height * 0.5;
+              context.drawImage(
+                watermark,
+                canvas.width - watermark.width + 160,
+                canvas.height - watermark.height + 195,
+                watermarkWidth,
+                watermarkHeight
+              );
+              context.fillStyle = "black";
+              context.font = "20px Arial";
+              context.fillText(`${"    "} Data Source ${source}`, 10, canvas.height - 10);
+              
+              const newDataURL = canvas.toDataURL();
+              const link = document.createElement('a');
+              link.download =`${fileName(meta)}.jpeg`;
+              link.href = newDataURL;
+              link.click();
+            };
+          };
         });
+
     }
   }
 
